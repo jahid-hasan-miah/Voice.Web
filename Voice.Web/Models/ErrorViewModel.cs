ab988afcae2e68cea090adbeb8820144b704a807
Md Jahid Hasan Miah <joyh304@gmail.com>
2021-12-24 20:01:06 +0600

    Add project files.

---
+++ Voice.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Voice.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
